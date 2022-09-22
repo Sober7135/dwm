8dae5ee9011411ed763e91f7f3f064337595895e
Sober7135 <soberwu@outlook.com>
2022-09-23 00:09:47 +0800

    update: change font and size.

--- dwm.c
+++ dwm.c
@@ -441,7 +441,7 @@ buttonpress(XEvent *e)
 		} else if (ev->x < x + blw)
 			click = ClkLtSymbol;
 		else if (ev->x > selmon->ww - (int)TEXTW(stext))
-			click = ClkStatusTextconst char *restrict format, ...;
+			click = ClkStatusText;
 		else
 			click = ClkWinTitle;
 	} else if ((c = wintoclient(ev->window))) {
